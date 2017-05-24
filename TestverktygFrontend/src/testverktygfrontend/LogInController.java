d6fe5188ced2729b1c3fa4c543adc137d7363f79
Anna Fock <anna@fock.se>
2017-05-24 14:11:27 +0200

    Skiss för scene log in klar

--- TestverktygFrontend/src/testverktygfrontend/LogInController.java
+++ TestverktygFrontend/src/testverktygfrontend/LogInController.java
@@ -10,21 +10,24 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
-/**
- *
- * @author annafock
- */
+
 public class LogInController implements Initializable {
     
     @FXML
-    private Label label;
+    private TextField textFieldUserName, textFieldPassword;
+    
+    @FXML private Label labelLogInMessage;
+    
+    @FXML private Button btnLogIn;
+    
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void handleLogInButton(ActionEvent event) {
+        
     }
     
     @Override
