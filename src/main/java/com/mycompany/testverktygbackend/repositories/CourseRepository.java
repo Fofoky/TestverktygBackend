bc0bc798b0cd747210661fecdb415610374167c3
Richard-Gustafsson <rille117@hotmail.com>
2017-05-24 14:45:41 +0200

    get Courses klar

--- src/main/java/com/mycompany/testverktygbackend/repositories/CourseRepository.java
+++ src/main/java/com/mycompany/testverktygbackend/repositories/CourseRepository.java
@@ -5,10 +5,32 @@
  */
 package com.mycompany.testverktygbackend.repositories;
 
+import com.mycompany.testverktygbackend.models.Course;
+import com.mycompany.testverktygbackend.models.User;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 /**
  *
  * @author rille
  */
 public class CourseRepository {
+    
+    public List<Course> getCourses(int userId){
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        
+        session.beginTransaction();
+        
+        User user = (User)session.get(User.class, userId);
+        List<Course> courses = user.getCourses();
+        session.getTransaction().commit();
+        session.close();
+        return courses;
+
+        
+    }
     
 }
--- src/main/java/com/mycompany/testverktygbackend/services/CourseService.java
+++ src/main/java/com/mycompany/testverktygbackend/services/CourseService.java
@@ -5,10 +5,19 @@
  */
 package com.mycompany.testverktygbackend.services;
 
+import com.mycompany.testverktygbackend.models.Course;
+import com.mycompany.testverktygbackend.repositories.CourseRepository;
+import java.util.List;
+
 /**
  *
  * @author rille
  */
 public class CourseService {
+    CourseRepository courseRepository = new CourseRepository();
+    
+    public List<Course> getCourses(int userId){
+        return courseRepository.getCourses(userId);
+    }
     
 }
