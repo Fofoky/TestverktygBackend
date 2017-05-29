855036b299c22e012d206f5ba6bc486583a719ae
Unknown <ht.uzpharma@gmail.com>
2017-05-29 14:01:05 +0500

    small changes in paths for getAllResponse, Path Param was missing

--- src/main/java/com/mycompany/testverktygbackend/resources/ResponseResource.java
+++ src/main/java/com/mycompany/testverktygbackend/resources/ResponseResource.java
@@ -7,6 +7,7 @@ import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -24,7 +25,7 @@ public class ResponseResource {
     ResponseService rr = new ResponseService();
 
 @GET    
-public List<Response> getAllResponse() {
+public List<Response> getAllResponse(@PathParam("questionId") int questionId) {
     return rr.getAllResponses();
 }    
 }
