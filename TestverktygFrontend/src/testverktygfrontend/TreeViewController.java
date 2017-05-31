6649f695570363b059a17b787669201903f9d253
Anna Fock <anna@fock.se>
2017-05-31 20:24:48 +0200

    Kommenterar fram rätt url och rätt kurs-id för att kunna testa

--- TestverktygFrontend/src/testverktygfrontend/TreeViewController.java
+++ TestverktygFrontend/src/testverktygfrontend/TreeViewController.java
@@ -118,7 +118,7 @@ public class TreeViewController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         logic = Logic.getInstance();
-        user = logic.getUser(2);
+        user = logic.getUser(1);
         loadTreeViewMenu();
     }
 
