1cac7076513a1f966d581e87dda04c66da7f8e08
Anna Fock <anna@fock.se>
2017-05-31 09:59:20 +0200

    ny commit utan ignore

--- TestverktygFrontend/src/testverktygfrontend/LogInController.java
+++ TestverktygFrontend/src/testverktygfrontend/LogInController.java
@@ -8,6 +8,8 @@ package testverktygfrontend;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -18,6 +20,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 
@@ -30,20 +33,35 @@ public class LogInController implements Initializable {
     
     @FXML private Button btnLogIn;
     
+    // Creating a static root to pass to the controller
+    private static BorderPane root = new BorderPane();
+
+    /**
+     * Just a root getter for the controller to use
+     */
+    public static BorderPane getRoot() {
+        return root;
+    }
     
+      
     @FXML
     private void handleLogInButton(ActionEvent event) throws IOException {
         
-        
-        ///HÄr ska nog namnet på anchorPanen stå istället.
-        
-        //Går till nästa scen
-        Parent root = FXMLLoader.load(getClass().getResource("Primary.fxml"));//skapar träd
+        //Definierar sökväg för menyn och fönstret
+        URL treeViewURL = getClass().getResource("TreeView.fxml");
+        URL welcomeUrl = getClass().getResource("Welcome.fxml");
 
-        Scene s = new Scene(root);//lägger in trädet i ett nytt objekt av klassen Scene
-        Stage stg = (Stage) ((Node) event.getSource()).getScene().getWindow();//Här hämtar vi den gamla scenen. source=button
-        stg.setScene(s);//byter gamla scenen mot den nya
-        
+            // definierar var i root som treeview respektive huvudfönstret ska ligga
+            root.setLeft(FXMLLoader.load(treeViewURL));
+
+            root.setCenter(FXMLLoader.load(welcomeUrl));
+
+       //Skapar ny scen av root och definierar måtten 
+        Scene scene = new Scene(root, 700, 500);
+
+        Stage stg = (Stage) ((Node) event.getSource()).getScene().getWindow();//Här hämtar nuvarande scen
+        stg.setScene(scene);//byter gamla stagen mot den nya      
+   
     }
     
     @Override
