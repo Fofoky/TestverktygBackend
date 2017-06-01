e40c72a77076063db2d71cd1756ead1fc16d8f4e
Wuz0ink <anton.alfsson@gmail.com>
2017-06-01 18:52:10 +0200

    dinner

--- TestverktygFrontend/src/testverktygfrontend/TeacherSelectedCourseController.java
+++ TestverktygFrontend/src/testverktygfrontend/TeacherSelectedCourseController.java
@@ -13,7 +13,12 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
 import testverktygfrontend.logic.Logic;
+import testverktygfrontend.model.Course;
+import testverktygfrontend.model.Question;
+import testverktygfrontend.model.QuestionOption;
+import testverktygfrontend.model.Response;
 import testverktygfrontend.model.Test;
+import testverktygfrontend.model.User;
 
 /**
  * FXML Controller class
@@ -24,15 +29,22 @@ public class TeacherSelectedCourseController implements Initializable {
     
     private Logic logic;
     private ObservableList<Test> testList;
+    private ObservableList<User> usersWithSelectedTest;
     
     @FXML
     private Label labelCourse;
     
     @FXML
-    private TableView<Test> tableTests, tableStudentTestResult;
+    private TableView<Test> tableTests;
     
     @FXML
-    private TableColumn<Test, String> columnTest, columnStatus, columnStudent, columnResult, columnStart, columnStop;
+    private TableView<User> tableStudentTestResult;
+    
+    @FXML
+    private TableColumn<Test, String> columnTest, columnStatus, columnStart, columnStop;
+    
+     @FXML
+    private TableColumn<User, String> columnStudent, columnResult;
     
     @FXML
     private Label labelSelectedTest;
@@ -50,6 +62,7 @@ public class TeacherSelectedCourseController implements Initializable {
         labelCourse.setText(logic.getSelectedCourse().getName());
         
         testList = FXCollections.observableArrayList();
+        usersWithSelectedTest = FXCollections.observableArrayList();
         
         logic.getSelectedCourse().getTests().forEach((a) -> {
             testList.add(a);
@@ -63,7 +76,44 @@ public class TeacherSelectedCourseController implements Initializable {
         columnStart.setCellFactory(TextFieldTableCell.forTableColumn());      
         columnStop.setCellFactory(TextFieldTableCell.forTableColumn());
          
-        tableTests.setItems(testList);
-    }    
+        tableTests.setItems(testList);   
+        
+        columnStudent.setCellValueFactory(new PropertyValueFactory<>("name"));
+        columnResult.setCellValueFactory(new PropertyValueFactory<>("resultSelectedTest"));
+        
+        columnStudent.setCellFactory(TextFieldTableCell.forTableColumn());
+        columnResult.setCellFactory(TextFieldTableCell.forTableColumn());
+        
+        tableStudentTestResult.setItems(usersWithSelectedTest);
+        
+        tableTests.getSelectionModel().selectedItemProperty().addListener((property, oldValue, newValue) -> {
+            usersWithSelectedTest.clear();
+            logic.setSelectedTest(newValue);
+
+            int studentResponse = 0;
+            for(User user : logic.getUsers()){
+                for(Course course : user.getCourses()){
+                    if(course.getCourseId() == logic.getSelectedCourse().getCourseId() && user.getUserRole().equals("Student")){
+                        
+                        for(Question question : logic.getSelectedTest().getQuestions()){
+                            for(QuestionOption option : question.getQuestionOptions()){
+                                if(option.isTrueFalse()){
+                                    for(Response response : question.getResponses()){
+                                        if(response.getResponse().equals(option.getQuestionOption()) && response.getUserId() == user.getUserId()){
+                                            studentResponse++;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        String result = studentResponse + "/" + logic.getSelectedTest().getQuestions().size();
+                        user.setResultOfSelectedTest(result);
+                        usersWithSelectedTest.add(user);
+                    }
+                }
+            }
+            
+        });
+    }   
     
 }
