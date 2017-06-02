2e013a82826e6c13e8d58e186eaaa7dbb95edfd4
Wuz0ink <anton.alfsson@gmail.com>
2017-06-02 10:35:56 +0200

    Skapat Question och Option converter

--- TestverktygFrontend/src/testverktygfrontend/CreateTestController.java
+++ TestverktygFrontend/src/testverktygfrontend/CreateTestController.java
@@ -1,12 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package testverktygfrontend;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -15,15 +14,18 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import testverktygfrontend.logic.Logic;
 import testverktygfrontend.model.Question;
 import testverktygfrontend.model.QuestionOption;
 
-/**
- * FXML Controller class
- *
- * @author annafock
- */
+
+
 public class CreateTestController implements Initializable {
+
+    private Logic logic;
+    private ObservableList<Object> questionList;
 
     @FXML
     Label labelCourse;
@@ -52,9 +54,50 @@ public class CreateTestController implements Initializable {
     @FXML
     Button buttonSaveTest; //Provet sparas egentligen ändå, men knappen leder tillbaka till kurssidan
 
+    @FXML
+    private void saveTest(ActionEvent event) throws IOException {
+        try {
+            String testName = textFieldTestName.getText().trim();
+
+        } catch (NullPointerException e) {
+
+        }
+
+    }
+
+    @FXML
+    private void addQuestion(ActionEvent event) throws IOException {
+
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        logic = Logic.getInstance();
+        labelCourse.setText(logic.getSelectedCourse().getName());
+        
+        questionList = FXCollections.observableArrayList();
+        
+        columnId.setCellValueFactory(new PropertyValueFactory<>("questionId"));
+        columnQuestion.setCellValueFactory(new PropertyValueFactory<>("question"));
+        
+        
+        columnOpt1.setCellValueFactory(new PropertyValueFactory<>("questionOption"));
+        columnOpt2.setCellValueFactory(new PropertyValueFactory<>("questionOption"));
+        columnOpt3.setCellValueFactory(new PropertyValueFactory<>("questionOption"));
+        columnOpt4.setCellValueFactory(new PropertyValueFactory<>("questionOption"));
+
+        columnId.setCellFactory(TextFieldTableCell.forTableColumn());
+        columnQuestion.setCellFactory(TextFieldTableCell.forTableColumn());
+        columnOpt1.setCellFactory(TextFieldTableCell.forTableColumn());
+        columnOpt2.setCellFactory(TextFieldTableCell.forTableColumn());
+        columnOpt3.setCellFactory(TextFieldTableCell.forTableColumn());
+        columnOpt4.setCellFactory(TextFieldTableCell.forTableColumn());
+        
+        
+        
+        tableCreateTest.setItems(questionList);
+
     }
 
 }
