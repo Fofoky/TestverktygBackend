dfbe0190254319bf5cbf1dc0970d62e34a6dc449
Unknown <rille117@hotmail.com>
2017-06-02 15:37:52 +0200

    Arbete på testsidan. Commit 1.

--- TestverktygFrontend/src/testverktygfrontend/TestController.java
+++ TestverktygFrontend/src/testverktygfrontend/TestController.java
@@ -6,7 +6,12 @@
 package testverktygfrontend;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -14,6 +19,10 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextArea;
+import testverktygfrontend.logic.Logic;
+import testverktygfrontend.model.Question;
+import testverktygfrontend.model.QuestionOption;
+import testverktygfrontend.model.Test;
 
 /**
  * FXML Controller class
@@ -21,7 +30,13 @@ import javafx.scene.control.TextArea;
  * @author annafock
  */
 public class TestController implements Initializable {
-
+    
+    private Logic logic;
+    private Test selectedTest;
+    private List<Question> questionList;
+    private List<QuestionOption> questionOptionList;
+    private List<QuestionOption> savedAnswers;
+    
     @FXML Label labelTestName, labelQuestionId, labelProgress;
     
     @FXML TextArea textAreaQuestion;
@@ -34,12 +49,117 @@ public class TestController implements Initializable {
     @FXML ProgressBar progressBar; //Går att koppla till property med bind
     
     
+    
+    public void handleButtonNextAction(ActionEvent event){
+        
+        
+    }
+    
+    ChangeListener checkIfChecked1 = new ChangeListener<Boolean>(){
+        @Override
+        public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+            if(newValue == true){
+                checkBox2.setDisable(true);
+                checkBox3.setDisable(true);
+                checkBox4.setDisable(true);
+            }
+            else{
+                checkBox2.setDisable(false);
+                checkBox3.setDisable(false);
+                checkBox4.setDisable(false);
+            }
+        }
+            
+    };
+    
+        ChangeListener checkIfChecked2 = new ChangeListener<Boolean>(){
+        @Override
+        public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+            if(newValue == true){
+                checkBox1.setDisable(true);
+                checkBox3.setDisable(true);
+                checkBox4.setDisable(true);
+            }
+            else{
+                checkBox1.setDisable(false);
+                checkBox3.setDisable(false);
+                checkBox4.setDisable(false);
+            }
+        }
+            
+    };
+        
+            ChangeListener checkIfChecked3 = new ChangeListener<Boolean>(){
+        @Override
+        public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+            if(newValue == true){
+                checkBox2.setDisable(true);
+                checkBox1.setDisable(true);
+                checkBox4.setDisable(true);
+            }
+            else{
+                checkBox2.setDisable(false);
+                checkBox1.setDisable(false);
+                checkBox4.setDisable(false);
+            }
+        }
+            
+    };
+            
+        ChangeListener checkIfChecked4 = new ChangeListener<Boolean>(){
+        @Override
+        public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+            if(newValue == true){
+                checkBox2.setDisable(true);
+                checkBox3.setDisable(true);
+                checkBox1.setDisable(true);
+            }
+            else{
+                checkBox2.setDisable(false);
+                checkBox3.setDisable(false);
+                checkBox1.setDisable(false);
+            }
+        }
+            
+    };        
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        checkBox1.selectedProperty().addListener(checkIfChecked1);
+        checkBox2.selectedProperty().addListener(checkIfChecked2);
+        checkBox3.selectedProperty().addListener(checkIfChecked3);
+        checkBox4.selectedProperty().addListener(checkIfChecked4);
+        
+        logic = Logic.getInstance();
+        selectedTest = logic.getSelectedTest();
+        questionList = new ArrayList();
+        questionOptionList = new ArrayList();
+        savedAnswers = new ArrayList();
+        
+        // Sparar de questions som finns för ett test i en ny lista.
+        selectedTest.getQuestions().forEach((a) -> {
+            questionList.add(a);
+            
+        });
+        
+        // Sparar de QuestionOptions som finns för alla frågor i en ny lista.
+        for(Question q : questionList){
+            for(QuestionOption qO : q.getQuestionOptions()){
+                questionOptionList.add(qO);
+                System.out.println(qO.getQuestionOption());
+            }
+        }
+        
+        
+        labelQuestionId.setText(String.valueOf(questionList.get(0).getQuestionId()));
+        textAreaQuestion.setText(questionList.get(0).getQuestion());
+        
+        labelTestName.setText(selectedTest.getTitle());
+        
+        
     }    
     
 }
